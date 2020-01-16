6a86adacd48dc112876ffed760d985c6b875c41f
DavidCorzo <davidcorzo@ufm.edu>
2020-01-16 08:00:15 -0600

    2020-01-16 Funciona lo de Santa Calculation: falta unit testing y profiler

---
+++ SantaCalculation/UnitTest/SantaCalculationTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SantaCalculationTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateMass() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
